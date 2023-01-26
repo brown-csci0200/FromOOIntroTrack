44f94a007a42592559e23aeccc739d9fd5a7005a
Kathi Fisler <kfisler@gmail.com>
2023-01-26 15:42:03 -0500

    lec1 oo setup

---
+++ src/Lec1Test.java
@@ -0,0 +1,21 @@
+package src;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
+public class Lec1Test {
+    /**
+     * Example test method.
+     * Test methods must have @Test before the method
+     */
+    @Test
+    public void testExample() {
+        // example syntax for assertEquals
+        Assert.assertEquals(1+1, 2);
+    }
+}
